223ee083ae2efceee346bb40a2aae7805bc4d9bc
Programador3 <Programador3@programador3>
2018-03-27 08:14:13 -0500

    limpia waRNINGS

--- src/main/java/com/prueba/rest/EgresoRest.java
+++ src/main/java/com/prueba/rest/EgresoRest.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.prueba.data.Deposito;
 import com.prueba.data.Egresos;
-import com.prueba.repository.IDeposito;
 import com.prueba.repository.IEgreso;
 
 @RestController("/egreso")
